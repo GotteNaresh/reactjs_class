c43aae60c95f62e8981539a69efad89b83fc255c
Rishi <dudise.purush1111@gmail.com>
2022-09-27 11:43:16 +0530

    01classcomponentchanges

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import ClassA from './components/ClassA';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import ClassA from "./components/ClassA";
 /* import User from './components/User'; */
-import ExportDiff, {A, B} from './components/ExportDiff';
-import LifecycleA from './components/lifecycle-methods-demo/LifecycleA';
-
-
-
+import ExportDiff, { A, B } from "./components/ExportDiff";
+import LifecycleA from "./components/lifecycle-methods-demo/LifecycleA";
 
 const App = () => {
-  const name = 'Naresh';
+  const name = "Naresh";
   const userText = <h2>Hello</h2>;
   //name = 'ramesh';
   const [name1, setName] = useState(name);
   const [currTimer, setCurrTimer] = useState(new Date());
-  const handleClick = () => setName('ramesh');
+  const handleClick = () => setName("ramesh");
   useEffect(() => {
     //setName('Suresh');
     setInterval(setCurrTimer(new Date()), 1000);
-  }, [currTimer])
-  
+  }, [currTimer]);
+
   return (
     <div className="App">
+
+      <h2> hello </h2>
+      
       <h1>Welcome</h1>
       {name1} - {userText}
       <h2>It is {new Date().toLocaleTimeString()}.</h2>
@@ -35,6 +35,6 @@ const App = () => {
       <LifecycleA></LifecycleA>
     </div>
   );
-}
+};
 
 export default App;
